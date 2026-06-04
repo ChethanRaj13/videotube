d1e58b896a60969d9a083d79885694cc9af6a3c2
ChethanRaj13 <chethanrajjee@gmail.com>
2026-06-04 21:10:03 +0530

    logic building

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,62 @@
-import {asyncHandler} from '../utils/asyncHandler.js';
+    import {asyncHandler} from '../utils/asyncHandler.js';
+    import { ApiError } from "../utils/ApiError.js";
+    import { user } from "../models/user.models.js";
+    import { uploadOnCloudinary } from "../utils/cloudinary.js";
+    import { ApiResponse } from '../utils/ApiResponse.js';
 
-const registerUser = asyncHandler(async (req, res) => {
-     res.status(200).json({
-        message: "Ok"
-    })
-});
+    const registerUser = asyncHandler(async (req, res) => {
+        const {fullname, email, username, password} = req.body;
+        
+        if (
+            [fullname,email,username,password].some((field) => 
+            field?.trim() === "")
+        ){
+            throw new ApiError(400,"All fields are required!")
+        }
 
-export {registerUser}
+        const existedUser =  user.findOne({
+            $or: [{ username },{ email }]
+        })
+
+        if(existedUser){
+            throw new ApiError(409,"user already exists!")
+        }
+
+        const avatarLocalPath = req.files?.avatar[0]?.path;
+        const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+        if (!avatarLocalPath){
+            throw new ApiError(400,"Avatar are required!")
+        }
+
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+        const converImage = await uploadOnCloudinary(coverImageLocalPath)
+
+        if(!avatar){
+            throw new ApiError(400,"Avatar are required!")
+        }
+
+        await user.create({
+                fullname,
+                avatar: avatar.url,
+                coverImage: converImage?.url || "",
+                email,
+                password,
+                username: username.toLowerCase()
+            })
+
+        const createdUser = await user.findById(user._id).select(
+            "-password -refreshToken"
+        )
+
+        if(!createdUser) {
+            throw new ApiError(500, "something went wrong!")
+        }
+
+        return res.status(201).json(
+            new ApiResponse(200,createdUser,"user registed successfully")
+        )
+
+    });
+
+    export {registerUser}
--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.use(cors({
     credential: true
 }))
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 import userRouter from "../routes/user.routes.js" 
 
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users",userRouter);
 
 
 export { app };
